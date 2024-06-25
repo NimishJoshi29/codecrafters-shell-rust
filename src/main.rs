ce30bca6b3faa16c71983535fece5b552c2e022d
NimishJoshi29 <joshinim29@gmail.com>
2024-06-25 19:01:53 +0530

    first stage

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    match stdin.read_line(&mut input) {
+        Ok(n) => println!("{} bytes read", n),
+        Err(e) => println!("{}", e.kind()),
+    }
+    println!("{}", input);
 }
